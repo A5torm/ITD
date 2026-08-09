50622c622f87a2544dee7c4f393d2823b0adcbb5
mirrorman <135953905+A5torm@users.noreply.github.com>
2026-08-10 02:29:25 +0700

    splendid sun pet

---
+++ Content/Buffs/PetBuffs/SplendidPaintingBuff.cs
@@ -0,0 +1,18 @@
+﻿using ITD.Content.Projectiles.Friendly.Pets;
+
+namespace ITD.Content.Buffs.PetBuffs;
+
+public class SplendidPaintingBuff : ModBuff
+{
+    public override void SetStaticDefaults()
+    {
+        Main.buffNoTimeDisplay[Type] = true;
+        Main.lightPet[Type] = true;
+    }
+
+    public override void Update(Player player, ref int buffIndex)
+    {
+        bool unused = false;
+        player.BuffHandle_SpawnPetIfNeededAndSetTime(buffIndex, ref unused, ModContent.ProjectileType<SplendidPaintingPet>());
+    }
+}
